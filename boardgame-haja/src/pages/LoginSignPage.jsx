35e2d57eee8933bc91ea3251e7c97cd0aabbb425
hyeri-woo <woohyeri0525@gmail.com>
2023-05-29 21:50:46 +0900

    refactor: Header 변경

--- boardgame-haja/src/pages/LoginSignPage.jsx
+++ boardgame-haja/src/pages/LoginSignPage.jsx
@@ -3,24 +3,16 @@ import { BigLogo } from '../components/common/Logo'
 import LoginForm from './form/LoginForm'
 import SignUpForm from './form/SignUpForm'
 import PageLayout from '../components/style/PageLayout'
-import NavLayout from '../components/style/NavLayout'
+import NavLayout from '../components/common/NavBar'
 import MainLayout from '../components/style/MainLayout'
+import Header from '../components/common/Header'
 
-export default function LoginSign() {
+export default function LoginSign({login}) {
     return (
         <PageLayout>
-            <header>
-                <BigLogo/>
-                <NavLayout>
-                    <ul>
-                        <li><button className="active">로그인</button></li>
-                        <li><button>회원가입</button></li>
-                    </ul>
-                </NavLayout>
-            </header>
+            <Header signin/>
             <MainLayout>
-                <LoginForm/>
-                {/* <SignUpForm/> */}
+                {login?<LoginForm/>:<SignUpForm/>}
             </MainLayout>
         </PageLayout>
         
