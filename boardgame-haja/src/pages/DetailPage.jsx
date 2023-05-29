1f967282a769f0f5cf0c3d0df535c4d54524aeaa
hyeri-woo <woohyeri0525@gmail.com>
2023-05-29 22:11:26 +0900

    rename: NavBar.jsx extension 수정

--- boardgame-haja/src/pages/DetailPage.jsx
+++ boardgame-haja/src/pages/DetailPage.jsx
@@ -1,42 +1,43 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import PageLayout from '../components/style/PageLayout'
-import NavLayout from '../components/style/NavLayout'
-import SearchBox from '../components/common/SearchBox'
-import LoginSign from '../components/common/LoginSign'
+import Header from '../components/common/Header'
+import {getDataById} from '../data/fetchData'
 import heartIcon from "../assets/image/empty-heart.svg"
-import {DetailHeader, DetailMainLayout, ImageWrapperStyle, InfoWrapperStyle} from "../components/style/DetailStyle"
+import {DetailMainLayout, ImageWrapperStyle, InfoWrapperStyle} from "../components/style/DetailStyle"
+import { useParams } from 'react-router-dom'
 
 
 export default function DetailPage() {
+  const [data, setData] = useState({});
+  const {id} = useParams();
+  useEffect(() => {
+    const getData = async() => {
+      const db = await getDataById(id);
+      setData(db);
+    }
+    getData();
+  }, []);
+  const player = `${data?.min_player}-${data?.max_player}명`
   return (
     <PageLayout>
-        <DetailHeader>
-            <SearchBox/>
-            <LoginSign/>
-            <button className="btn-back" type="button">
-              &#60; 목록으로 돌아가기
-            </button>
-            <NavLayout>
-              <h2>상세정보</h2>
-            </NavLayout>
-        </DetailHeader>
+        <Header search/>
         <DetailMainLayout>
             <ImageWrapperStyle>
-              <img src="image" className="main-image"/>
+              <img src={data?.image} className="main-image"/>
               <button type="button"><img src={heartIcon} className="heart-image"/>찜하기</button>
             </ImageWrapperStyle>
             <InfoWrapperStyle>
               <div class="title-wrapper">
-                <span className="ranking">#91</span>
-                <h3 style={{fontSize:"40px", fontWeight:"bold"}}>달무티</h3>
+                <span className="ranking">#{data?.ranking}</span>
+                <h3 style={{fontSize:"40px", fontWeight:"bold"}}>{data?.name}</h3>
               </div>
               <ul>
-                <li>인원<span>1-4명</span></li>
-                <li>플레이 타임<span>15분</span></li>
-                <li>사용 연령<span>12세 이상</span></li>
-                <li>테마<span>공상 과학</span></li>
-                <li>난이도<span>3.15/5</span></li>
-                <li>평점<span>8.9/10</span></li>
+                <li>인원<span>{player}</span></li>
+                <li>플레이 타임<span>{data?.play_time}</span></li>
+                <li>사용 연령<span>{data?.play_age}</span></li>
+                <li>테마<span>{data?.theme}</span></li>
+                <li>난이도<span>{data?.difficulty}/5</span></li>
+                <li>평점<span>{data?.rate}/10</span></li>
               </ul>
             </InfoWrapperStyle>
         </DetailMainLayout>
