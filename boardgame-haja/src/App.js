1e5ddb90c46827d1bbeb62f539143bc5541c5ee8
hyeri-woo <woohyeri0525@gmail.com>
2023-05-29 21:41:34 +0900

    feat: routes 생성

--- boardgame-haja/src/App.js
+++ boardgame-haja/src/App.js
@@ -1,34 +1,12 @@
-import { useEffect, useState } from "react";
 import GlobalStyle from './components/style/GlobalStyle'
 import Footer from './components/common/Footer'
-import LoginSignPage from "./pages/LoginSignPage";
-import HomePage from "./pages/HomePage";
-import DetailPage from "./pages/DetailPage"
+import Router from './routes/Router'
 function App() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3001/game');
-        if(!response.ok) {
-            throw new Error("failed to retrieve boardgame.json");
-        }
-        const json = await response.json();
-        setData(json)
-      } catch(error) {
-          console.error(error);
-      }
-    }
-    fetchData();
-  }, []);
-  console.log(data);
   return (
     <>
       <GlobalStyle/>
       <div className="wrapper">
-        {/* <HomePage/> */}
-        <DetailPage/>
-        {/* <LoginSignPage/> */}
+        <Router/>
       </div>
       <Footer/>
     </>
